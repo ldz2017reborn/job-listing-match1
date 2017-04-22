1b601db602c1749a5436e4812fed46daae55f3d2
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-04-22 16:14:01 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,5 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  before_action :require_is_admin
 
   def require_is_admin
      if !current_user.admin?
