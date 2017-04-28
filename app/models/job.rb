632d71e245fabf286efdfb9bb3b0255863b03c2c
刘德政 <liudezheng@liudezhengdeMacBook-Pro.local>
2017-04-28 22:41:47 +0800

    add location column

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,7 @@ class Job < ApplicationRecord
   validates :title, presence: true
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
+  validates :location, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
 
   scope :published, -> { where(is_hidden: false) }
